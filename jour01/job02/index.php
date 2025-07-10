ba13e9f2f947a63ff1221e8f026e0fc68a7d9729
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-07-10 10:03:11 +0200

    job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str1 = "Hello";
+$str2 = "LaPlateforme!"
+
+echo $str1 . " " . $str2; //premier affichage de la phrase demandé
+
+echo $str1 . " " . $str2; 
+/*deuxième commentaire de 
+manière différentes*/
+?>
