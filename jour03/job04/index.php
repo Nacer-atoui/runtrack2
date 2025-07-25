a394e2c28fd9647114b1e6d2bf21dce1107b9284
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-07-25 14:44:07 +0200

    fin du job04

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,6 +1,27 @@
 <?php
 
+$str = "Dans l'espace, personne ne vous entend crier";
 
+function countCarac($str){
+$i = 0;
+while (isset($str[$i])){
+    $i++;
+}
+return $i;
+}
+
+$result = countCarac($str);
+echo "le nombre de cractère dans la phrase est de : " .$result;
+
+
+
+
+
+// foreach ($str as $value){
+//     $x++; 
+// }
+
+//     echo "le nombre de caractere dans la phrase est de : " . $x;
 
 
 ?>
