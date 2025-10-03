7bc3b931200ec095701370a4b6d8bcb82b51bcdf
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-10-03 10:10:30 +0200

    fin jour10 job jusqu'à job06

--- jour10/job05/index.php
+++ jour10/job05/index.php
@@ -3,7 +3,7 @@
     $pdo = new PDO("mysql:host=localhost;dbname=jour09;charset=utf8", "root", "");
 
     //requete pour importer les données de ma table de données
-    $requete = $pdo->query("SELECT * FROM etudiants WHERE prenom LIKE 'T%'");
+    $requete = $pdo->query("SELECT * FROM etudiants WHERE TIMESTAMPDIFF(YEAR, naissance, CURDATE()) < 18;");
 
     $etudiants = $requete->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour 10 job04</title>
+    <title>Jour 10 job05</title>
 </head>
 <body>
     <table border="1px solid black">
--- jour10/job06/index.php
+++ jour10/job06/index.php
@@ -3,7 +3,8 @@
     $pdo = new PDO("mysql:host=localhost;dbname=jour09;charset=utf8", "root", "");
 
     //requete pour importer les données de ma table de données
-    $requete = $pdo->query("SELECT * FROM etudiants WHERE prenom LIKE 'T%'");
+    $requete = $pdo->query("SELECT COUNT(*) AS nb_etudiants
+FROM etudiants;");
 
     $etudiants = $requete->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,28 +15,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour 10 job04</title>
+    <title>Jour 10 job06</title>
 </head>
 <body>
     <table border="1px solid black">
         <thead>
             <tr>
 
-                <th>Prénom</th>
-                <th>Nom</th>
-                <th>Naissance</th>
-                <th>Sexe</th>
-                <th>Email</th>
+                <th>Nombre d'étudiants</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($etudiants as $etudiant): ?>
             <tr>
-                <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
-                <td><?= htmlspecialchars($etudiant['nom']) ?></td>
-                <td><?= htmlspecialchars($etudiant['naissance']) ?></td>
-                <td><?= htmlspecialchars($etudiant['sexe']) ?></td>
-                <td><?= htmlspecialchars($etudiant['email']) ?></td>
+                <td><?= htmlspecialchars($etudiant['nb_etudiants']) ?></td>
             </tr>
             <?php endforeach; ?>
         </tbody>
