3c7be005613c65747670dca3a3f66e018e7c2f2c
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-10-03 10:01:14 +0200

    fin jour10 job04

---
+++ jour10/job03/index.php
@@ -0,0 +1,58 @@
+<?php 
+//connexion à la base données
+    $pdo = new PDO("mysql:host=localhost;dbname=jour09;charset=utf8", "root", "");
+
+    //requete pour importer les données de ma table de données
+    $requete = $pdo->query("SELECT prenom, nom, naissance FROM etudiants WHERE sexe = 'Femme'");
+
+    $etudiants = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 10 job03</title>
+</head>
+<body>
+    <table border="1px solid black">
+        <thead>
+            <tr>
+
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Naissance</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($etudiants as $etudiant): ?>
+            <tr>
+                <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['nom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['naissance']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+    <style>
+        table {
+    border-collapse: collapse;
+    width: 80%;
+}
+
+th, td {
+    border: 1px solid #333;
+    padding: 8px;
+    text-align: left;
+}
+
+thead {
+    background-color: #a6c1efff;
+    
+}
+    </style>
+</body>
+</html>
---
+++ jour10/job04/index.php
@@ -0,0 +1,62 @@
+<?php 
+//connexion à la base données
+    $pdo = new PDO("mysql:host=localhost;dbname=jour09;charset=utf8", "root", "");
+
+    //requete pour importer les données de ma table de données
+    $requete = $pdo->query("SELECT * FROM etudiants WHERE prenom LIKE 'T%'");
+
+    $etudiants = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1px solid black">
+        <thead>
+            <tr>
+
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Naissance</th>
+                <th>Sexe</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($etudiants as $etudiant): ?>
+            <tr>
+                <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['nom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['naissance']) ?></td>
+                <td><?= htmlspecialchars($etudiant['sexe']) ?></td>
+                <td><?= htmlspecialchars($etudiant['email']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+    <style>
+        table {
+    border-collapse: collapse;
+    width: 80%;
+}
+
+th, td {
+    border: 1px solid #333;
+    padding: 8px;
+    text-align: left;
+}
+
+thead {
+    background-color: #a6c1efff;
+    
+}
+    </style>
+</body>
+</html>
